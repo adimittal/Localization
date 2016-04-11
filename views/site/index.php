7f137cb1ef87d46bdccd218fc75c38c8d7b803e7
Aditya Mittal <scientificchess@gmail.com>
2016-04-11 11:26:28 -0700

    making the readme more dynamic to get the resource files

--- views/site/index.php
+++ views/site/index.php
@@ -22,11 +22,17 @@ $this->title = 'Saas Localization';
         <div class="row">
             
             <?php
+            switch($SERVER['SERVER_NAME']) {
+              case 'localization.dev.itsonsaas.net' :
+                $host = "http://localization.dev.itsonsaas.net:8000";
+              case 'hercules.dev.itsoninc.com' :
+                $host = "http://hercules.dev.itsoninc.com:55578";
+            }
             echo <<<HT
               <br /><br />
               <div class="col-lg-3">
                 <h3>Common links for all projects</h3>
-                <p><a class="btn btn-primary" href="/translation/readme?project=portal&host=http://localization.dev.itsonsaas.net:8000">Readme for Curl calls</a></p>
+                <p><a class="btn btn-primary" href="/translation/readme?project=portal&host=$host">Readme for Curl calls</a></p>
                 <p><a class="btn btn-primary" href="/translation/upload">Upload To Saas-Localization</a></p>
                 <p><a class="btn btn-info" href="/translation/download">Download from Saas-Localization</a></p>
             </div>
