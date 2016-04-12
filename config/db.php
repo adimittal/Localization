35d0b49d1088847c3dafa011b706a9c94f617361
Aditya Mittal <scientificchess@gmail.com>
2016-04-12 16:12:46 -0700

    use env instead of my local settings for database

--- config/db.php
+++ config/db.php
@@ -1,9 +1,14 @@
 <?php
 
+$host = getenv('mysql_host');
+$user = getenv('mysql_user');
+$pass = getenv('mysql_pass');
+
+
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => "mysql:host=$host;dbname=saas_loc",
+    'username' => $user,
+    'password' => $pass,
     'charset' => 'utf8',
 ];
